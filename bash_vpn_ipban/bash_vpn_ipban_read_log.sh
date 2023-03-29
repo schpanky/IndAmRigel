3b9d39ac3b30e96cec4f29ef0f7b2754b1adf8eb
schpanky <schpanky@gmail.com>
2023-03-29 08:43:31 -0400

    Without ITE verified as IP:port output

--- bash_vpn_ipban/bash_vpn_ipban_read_log.sh
+++ bash_vpn_ipban/bash_vpn_ipban_read_log.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+#	Read openvpn.log for IP/Ports
+#	README:
+#	Set File_Path to your openvpn.log
+#
+#	from command line, run:
+#	sed -i 's/\r//' setup.sh to sanitize before running
+#	if you run into the error:
+#	syntax error near unexpected token `$'\r''
+#
+#
 FILE_PATH="/etc/openvpn/openvpn.log"
-IP_ADDRESSES=( $(grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]+' $FILE_PATH) )
-for ip in "${IP_ADDRESSES[@]}"; do
-  echo "IP Addresses in log: $ip"
+IP_ADDRESSES=( $(grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]+' $FILE_PATH|sort|uniq) )
+for ip in "${IP_ADDRESSES[@]}";
+do echo "IP Addresses in log: $ip"
 done
--- bash_vpn_ipban/bash_vpn_ipban_read_log_ITE.sh
+++ bash_vpn_ipban/bash_vpn_ipban_read_log_ITE.sh
@@ -1,12 +1,27 @@
+
 #!/bin/bash
+#bash script to openvpn.log
+
+# Define the file path
 FILE_PATH="/etc/openvpn/openvpn.log"
 
+# Check if the file exists and is readable
 if [[ -r $FILE_PATH ]]; then
-    ip_addresses=(grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]+' /etc/openvpn/openvpn.log|sort|uniq))
+    # Read the content of the file into a variable named FILE_CONTENT
+    FILE_CONTENT=$(cat "$FILE_PATH")
+
+    # Debug: Print the content of the variable
+    # echo "Content of the file:"
+    # echo "$FILE_CONTENT"
+	
+		# set variable $ip_addresses to results of $file_content filtered to unique IP entries
+		ip_addresses=($(echo "$FILE_CONTENT" | grep -oP '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b' | sort | uniq))
+
     
-    for ip in "${ip_addresses[@]}"; do
+	for ip in "${ip_addresses[@]}"; do
         echo "IP Addresses in log: $ip"
     done
 else
     echo "Error: Log file does not exist or is not readable."
 fi
+
