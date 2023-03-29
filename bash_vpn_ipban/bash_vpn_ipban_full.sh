a379bfb81f3f66f57e96e459311b98cad7f0f920
schpanky <schpanky@gmail.com>
2023-03-29 09:29:11 -0400

    Fully working and tested copy v0.01

---
+++ bash_vpn_ipban/bash_vpn_ipban_full.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#   Goal:
+#   Read openvpn.log for IP's on lines containing hack attempts
+#	Then add to IPTABLES deny
+#
+#   README:
+#
+#	!! Requires root/su permissions !!
+#   Set FILE_PATH to your openvpn.log
+#
+#   from command line, run:
+#   sudo sed -i 's/\r//' setup.sh
+#   to sanitize as unix carriage returns
+#
+#   sudo chmod +x bash_vpn_ipban_full.sh
+#   to enable ability to execute
+#
+#
+
+FILE_PATH="/etc/openvpn/openvpn.log"
+IPS2BAN=$(grep "WARNING: Bad encapsulated packet length from peer" $FILE_PATH | sed -nE 's/.*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}):[0-9]{1,5}.*/\1/p'|sort|uniq)
+
+while read -r ip; do
+
+#Skip empty lines and lines starting with '#'
+[[ -z "${ip}" || "${ip}" =~ ^# ]] && continue
+
+#Add the IP to the INPUT chain deny list
+iptables -A INPUT -s "${ip}" -j DROP
+
+#Add the IP to the FORWARD chain deny list (if necessary)
+iptables -A FORWARD -s "${ip}" -j DROP
+
+echo "Added ${ip} to iptables deny list"
+done <<< "${IPS2BAN}"
+
+#Save iptables rules
+#touch /etc/iptables/rules.v4.automated.txt
+#iptables-save > /etc/iptables/rules.v4.automated.txt
+
+echo "All IPs added to iptables deny list successfully."
